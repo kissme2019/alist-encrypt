9f396de1050426495c2b593e04f6bf21cbb5fa61
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2023-04-27 23:51:00 +0800

    Update encDavHandle.js

--- node-proxy/src/encDavHandle.js
+++ node-proxy/src/encDavHandle.js
@@ -137,11 +137,11 @@ const handle = async (ctx, next) => {
     // check dir, convert url
     const fileName = path.basename(url)
      //处理文件夹请求
-    if (fileName.indexOf('.') == -1 && url.endsWith("/") && ~'GET'.indexOf(request.method.toLocaleUpperCase()))
-    {
-       await next()
-       return
-    }
+//     if (fileName.indexOf('.') == -1 && url.endsWith("/") && ~'GET'.indexOf(request.method.toLocaleUpperCase()))
+//     {
+//        await next()
+//        return
+//     }
     
     const realName = convertRealName(passwdInfo.password, passwdInfo.encType, url)
     request.url = url.replace(fileName, realName)
